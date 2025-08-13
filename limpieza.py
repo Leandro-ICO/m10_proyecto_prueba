756e8e52846aac2ac1d489665af6b5be74aaef72
LeoCGZ <leandro.ingcomercial@gmail.com>
2025-08-12 23:06:17 -0400

    Primera etapa terminada, en proceso de segunda etapa

---
+++ limpieza.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+from thefuzz import fuzz, process
+
+# Definir el directorio de trabajo (lugar en el cual se encuentran los archivos)
+
+directorio = r"C:\Users\leoac\Desktop\Análisis de datos (Avanzado)\Modulo 10\proyecto"
+
+os.chdir(directorio) #asignar directorio (change directorio)
+
+#print("Directorio de trabajo", os.getcwd()) verificar directorio de trabajo
+
+df_ventas = pd.read_csv("Ventas.csv")
+
+df_vendedores = pd.read_csv("Vendedores.csv")
+
+# print(df_ventas.info())
+# print(df_vendedores.info())
+
+#Limpiar nombres de empresas
+df_ventas["empresa"] = df_ventas["empresa"].str.lower().str.strip() #lower minusculas y elimina espacios al final y al inicio
+df_vendedores["empresa"] = df_vendedores["empresa"].str.lower().str.strip()
+
+def encontrar_mejor_match(nombre,lista_empresas):
+    mejor_match, score = process.extractOne(nombre, lista_empresas, scorer=fuzz.token_sort_ratio) #extrae nombre coincidente con lista_empresas y le asigna un score de coincidencia
+    
+    # print(score)
+    return mejor_match if score > 50 else None
+
+#recorre empresa de df_ventas y aplica la función lambda encontrando mejor match, donde x es el nombre de la empresa en df_ventas
+#y compara con empresa de df_vendedores en formato lista
+df_ventas["empresa_corregida"] = df_ventas["empresa"].apply(lambda x: encontrar_mejor_match(x, df_vendedores["empresa"].tolist()))
+
+# print(df_ventas.head(10))
+
+df_final = df_ventas.merge(df_vendedores,left_on="empresa_corregida",right_on="empresa", how="left").drop(columns=["empresa_y"])
+
+#corregir nombres de las columnas
+df_final.rename(columns={"empresa_x":"empresa_original"}, inplace=True)
+
+# print(df_final.head())
+
+df_sin_match = df_final[df_final["empresa_corregida"].isna()]
+
+print(df_sin_match.head())
+
+#Guardar los reportes de los 2 dataframes
+
+df_final.to_csv("resultados_cruce.csv",index=False)
+df_sin_match.to_csv("registros_sin_cruce.csv",index=False)
+
+#pendiente segunda etapa de limpieza de datos
